50456da551f33bd4354f12bd115100add29d82b2
shabai517 <shabai517@163.com>
2020-02-10 16:28:26 +0000

    add resource component

--- src/views/landingpage/LandingPage.vue
+++ src/views/landingpage/LandingPage.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="landingpage-container">
       <div class="panel nav"><NavBar page="landingpage"/></div>
-    <!--   <div class="panel resource"><Resource/></div>
-      <div class="panel statistics"><Statistics/></div>
+      <div class="panel resource"><Resource/></div>
+     <!--  <div class="panel statistics"><Statistics/></div>
       <div class="panel tools"><Tools/></div>
       <div class="panel news"><News/></div> -->
   </div>
@@ -10,7 +10,7 @@
 
 <script>
   import NavBar from '@/components/Nav'
-  // import Resource from '@/components/landingpage/Resource'
+  import Resource from '@/components/landingpage/Resource'
   // import Statistics from '@/components/landingpage/Statistics'
   // import Tools from '@/components/landingpage/Tools'
   // import News from '@/components/landingpage/News'
@@ -26,7 +26,7 @@
     },
     components: {
       NavBar,
-      // Resource,
+      Resource,
       // Statistics,
       // Tools,
       // News
@@ -40,12 +40,6 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>  
   .panel.nav{
-    /*position: absolute;
-    top: 37px;
-    left: 0;
-    right: 0;
-    z-index: 1000;
-    */
     border-bottom: 1px solid #f3f3f3;
     box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
     z-index: 1000;
@@ -54,10 +48,8 @@
   .panel.resource{
     background-color: #fbfdff;
     border-bottom: 1px solid #f3f3f3;
-    /*box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);*/
   }
   .panel.statistics{
-    /*background: #f7f8fa;*/
     border-bottom: 1px solid #f3f3f3;
   }
   .panel.tools{
@@ -66,8 +58,6 @@
     
   }
   .panel.news{
-    
     border-bottom: 1px solid #f3f3f3;
-    /*background: #f7f8fa;*/
   }
 </style>
